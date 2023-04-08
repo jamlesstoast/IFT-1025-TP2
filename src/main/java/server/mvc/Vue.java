1db0d1fe54a40064b3baec12133d11eedd3b312a
CelinaZhang11 <115120298+CelinaZhang11@users.noreply.github.com>
2023-04-08 11:03:51 -0400

    New changes: added vertical separator and a place holder for when table is empty.

--- src/main/java/server/mvc/Vue.java
+++ src/main/java/server/mvc/Vue.java
@@ -12,6 +12,7 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
+import javafx.geometry.Insets;
 
 public class Vue extends Application {
 
@@ -26,7 +27,6 @@ public class Vue extends Application {
         VBox rightVertical = new VBox();
         VBox leftVertical = new VBox();
         HBox leftHorizontal = new HBox();
-
         BorderPane root = new BorderPane();
         Scene scene = new Scene(root, 100, 100, Color.BEIGE);
 
@@ -37,16 +37,19 @@ public class Vue extends Application {
         registrationForm.setVgap(10);
         registrationForm.setPadding(new Insets(25, 25, 25, 25));
 
+        // creating textfields
         TextField textFieldP = new TextField();
         TextField textFieldN = new TextField();
         TextField textFieldE = new TextField();
         TextField textFieldM = new TextField();
 
+        // creating labels
         Label labelP = new Label("Prénom");
         Label labelN = new Label("Nom");
         Label labelE = new Label("Email");
         Label labelM = new Label("Matricule");
 
+        // add form fields
         registrationForm.add(new Text("Formulaire d'inscription"), 0, 0);
         registrationForm.add(labelP, 0, 1);
         registrationForm.add(textFieldP, 1, 1);
@@ -56,6 +59,11 @@ public class Vue extends Application {
         registrationForm.add(textFieldE, 1, 3);
         registrationForm.add(labelM, 0, 4);
         registrationForm.add(textFieldM, 1, 4);
+
+        // set layout for the form
+        registrationForm.setHgap(10);
+        registrationForm.setVgap(10);
+        registrationForm.setPadding(new Insets(10, 10, 10, 10));
 
         // Creating buttons
         Button charger = new Button("charger");
@@ -81,7 +89,7 @@ public class Vue extends Application {
         courseColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 
         courseTable.getColumns().addAll(courseColumn, codeColumn);
-
+        courseTable.setPlaceholder(new Label("No content in table"));
 
         // Creating dropdown list
         ComboBox<String> sessionList = new ComboBox<>();
@@ -91,7 +99,7 @@ public class Vue extends Application {
                 "Été"
         );
 
-        // Liste des cours
+        // Liste des cours (left)
         leftVertical.getChildren().add(0, new Text("Liste des cours"));
         leftVertical.getChildren().add(courseTable);
         leftVertical.getChildren().add(sepHorizontal);
@@ -99,13 +107,13 @@ public class Vue extends Application {
         leftHorizontal.getChildren().add(0, sessionList);
         leftHorizontal.getChildren().add(charger);
 
-        // Formulaire d'inscription
+        // Formulaire d'inscription (right)
         rightVertical.getChildren().add(0, registrationForm);
         rightVertical.getChildren().add(envoyer);
 
         // Dividing the screen
         SplitPane splitPane = new SplitPane();
-        splitPane.getItems().addAll(leftVertical, rightVertical);
+        splitPane.getItems().addAll(leftVertical, sepVertical, rightVertical);
 
         primaryStage.setTitle("Inscription UdeM");
         primaryStage.setScene(scene);
