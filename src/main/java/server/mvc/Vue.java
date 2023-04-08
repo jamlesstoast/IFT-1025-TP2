3d74c3223c9a1e01c9b9ee6b7fcc47ac8e2be887
CelinaZhang11 <115120298+CelinaZhang11@users.noreply.github.com>
2023-04-07 21:02:50 -0400

    New changes: dropdown list and table

--- src/main/java/server/mvc/Vue.java
+++ src/main/java/server/mvc/Vue.java
@@ -1,16 +1,13 @@
 package server.mvc;
+import server.models.Course;
 
 import javafx.application.Application;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
@@ -32,7 +29,6 @@ public class Vue extends Application {
 
         BorderPane root = new BorderPane();
         Scene scene = new Scene(root, 100, 100, Color.BEIGE);
-
 
         // Creating empty text fields
         TextField textFieldP = new TextField();
@@ -60,11 +56,30 @@ public class Vue extends Application {
         Separator sepVertical = new Separator();
         sepVertical.setOrientation(Orientation.VERTICAL);
 
+        // Creating table
+        TableView<Course> tableView = new TableView<>();
+
+        // Creating dropdown list
+        ComboBox<String> dropDown = new ComboBox<>();
+
         // Liste des cours
         leftVertical.getChildren().add(0, new Text("Liste des cours"));
-        leftVertical.getChildren().add(new Text("Code"));
-        leftHorizontal.getChildren().add(new Text("Cours"));
+
+        TableColumn<Course, String> codeColumn = new TableColumn<>("Code");
+        codeColumn.setCellValueFactory(new PropertyValueFactory<>("code"));
+
+        TableColumn<Course, String> nameColumn = new TableColumn<>("Cours");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        tableView.getColumns().addAll(codeColumn, nameColumn);
+
+
         root.getChildren().add(sepHorizontal);
+        dropDown.getItems().addAll(
+                "Hiver",
+                "Automne",
+                "Été"
+        );
         leftVertical.getChildren().add(charger);
 
         // Formulaire d'inscription
