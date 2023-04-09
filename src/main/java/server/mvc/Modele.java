552152174f155bcec4c9fadeed7dba96028c0d13
CelinaZhang11 <115120298+CelinaZhang11@users.noreply.github.com>
2023-04-09 17:26:41 -0400

    New changes: 'Inscription'

--- src/main/java/server/mvc/Modele.java
+++ src/main/java/server/mvc/Modele.java
@@ -1,16 +1,20 @@
 package server.mvc;
 import server.models.Course;
+import server.models.RegistrationForm;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Modele {
 
-    private List<Course> courses;
+    private final List<Course> courses;
 
     public Modele(){
         courses = new ArrayList<>();
@@ -42,7 +46,52 @@ public class Modele {
             bufferedreader.close();
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println("Registration failed due to missing file 'inscription.txt'");
         }
         return courses;
     }
-}
+
+    public void addRegistration(RegistrationForm form) throws IllegalArgumentException {
+
+        // Validate session -> 3 choices
+        if (!form.getCourse().getSession().equals("Hiver") && !form.getCourse().getSession().equals("Automne")
+                && !form.getCourse().getSession().equals("Été")) {
+            throw new IllegalArgumentException("Invalid session");
+        }
+
+        // Validate course code -> IFT1025
+        if (!form.getCourse().getCode().matches("[A-Z]{3}\\d{4}")) {
+            throw new IllegalArgumentException("Invalid course code");
+        }
+
+        // Validate matricule -> 20207461
+        if (!form.getMatricule().matches("\\d{8}")) {
+            throw new IllegalArgumentException("Invalid matricule");
+        }
+
+        // Validate first name
+        if (!form.getPrenom().matches("^[a-zA-ZÀ-ÿ\\\\-]+$")) {
+            throw new IllegalArgumentException("Invalid first name");
+        }
+
+        // Validate last name
+        if (!form.getNom().matches("^[a-zA-ZÀ-ÿ\\-]+$")) {
+            throw new IllegalArgumentException("Invalid last name");
+        }
+
+        // Validate email -> celina.zhang@umontreal.ca
+        if (!form.getEmail().matches("/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$/")) {
+            throw new IllegalArgumentException("Invalid email");
+        }
+
+        // Write registration to 'inscription.txt' file
+        String line = form.getCourse().getSession() + "\t" + form.getCourse().getCode() + "\t" +
+                form.getMatricule() + "\t" + form.getPrenom() + "\t" + form.getNom() + "\t" + form.getEmail();
+
+        try {
+            Files.write(Paths.get("inscription.txt"), (line + System.lineSeparator()).getBytes(),
+                    StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }}
