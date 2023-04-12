5ab4d3ecb14af5edb3d22a13145fc99aa33229a7
jamlesstoast <yang.julie22@gmail.com>
2023-04-12 14:50:06 -0400

    Javadoc HTML files

--- src/main/java/server/EventHandler.java
+++ src/main/java/server/EventHandler.java
@@ -1,13 +1,13 @@
 package server;
 
 /**
- * Assure la gestion des evènements pour toutes les classes qui l'implementent
+ * Assure la gestion des evenements pour toutes les classes qui l'implementent
  */
 @FunctionalInterface
 public interface EventHandler {
     /**
      * Methode de gestion de commande
-     * @param cmd La commande à gerer
+     * @param cmd La commande a gerer
      * @param arg L'argument de la commande
      */
     void handle(String cmd, String arg);
