4b998ec19692ce6239f27a465c39700722acf98b
CelinaZhang11 <115120298+CelinaZhang11@users.noreply.github.com>
2023-04-13 18:06:04 -0400

    envoyer()

--- src/main/java/mvc/Controleur.java
+++ src/main/java/mvc/Controleur.java
@@ -3,37 +3,21 @@ package mvc;
 import server.models.Course;
 import server.models.RegistrationForm;
 
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.cell.PropertyValueFactory;
-
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.util.*;
 import javafx.scene.control.*;
 
 public class Controleur {
     private Vue vue;
     private Modele modele;
-    private ComboBox<String> sessionList;
-    private TableView<Course> courseTable;
-    private TableColumn<Course, String> courseColumn;
-    private TableColumn<Course, String> codeColumn;
-    private ObjectInputStream objectInputStream;
 
-
-    public Controleur(Vue vue, Modele modele, ComboBox<String> sessionList, TableView<Course> courseTable,
-                      TableColumn<Course, String> courseColumn, TableColumn<Course, String> codeColumn,
-                      ObjectInputStream objectInputStream) {
+    public Controleur(Vue vue, Modele modele) {
         this.vue = vue;
         this.modele = modele;
-        this.sessionList = sessionList;
-        this.courseTable = courseTable;
-        this.courseColumn = courseColumn;
-        this.codeColumn = codeColumn;
-        this.objectInputStream = objectInputStream;
 
 
-        this.modele.getChargerButton().setOnAction((action) -> {
+        this.vue.getChargerButton().setOnAction((action) -> {
             try {
                 System.out.println("Bouton 'charger' a été cliqué");      // for testing purposes, we can remove this
                 this.charger();
@@ -42,7 +26,7 @@ public class Controleur {
             }
         });
 
-        this.modele.getEnvoyerButton().setOnAction((action) -> {
+        this.vue.getEnvoyerButton().setOnAction((action) -> {
             try {
                 System.out.println("Bouton 'envoyer' a été cliqué");      // for testing purposes, we can remove this
                 this.envoyer();
@@ -59,46 +43,52 @@ public class Controleur {
      */
     private void charger() throws IOException, ClassNotFoundException {
         // Button clicked -> Modele is called
-        String selectedSemester = sessionList.getValue();
+        String selectedSemester = Vue.getSessionList().getValue();
         List<Course> courses = modele.loadCourses(selectedSemester);
 
         // Clear existing data from table (if new semester is chosen)
-        courseTable.getItems().clear();
+        Vue.getCourseTable().getItems().clear();
 
         // Add new data to table
-        courseTable.getItems().addAll(courses);
-        codeColumn.setCellValueFactory(new PropertyValueFactory<>("code"));
-        courseColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        Vue.getCourseTable().getItems().addAll(courses);
+        Vue.getCodeColumn().setCellValueFactory(new PropertyValueFactory<>("code"));
+        Vue.getCourseColumn().setCellValueFactory(new PropertyValueFactory<>("name"));
     }
 
     private void envoyer() throws IOException {
         // Button clicked -> Modele is called
-        String semester = sessionList.getValue();
-        modele.registerStudent();
+        String selectedSemester = Vue.getSessionList().getValue();
+        modele.registerStudent(selectedSemester);
 
-        // Add event handler to courseTable -> course needs to be chosen for registration form to be sent
-        courseTable.setOnMouseClicked(event -> {
-            // Get the selected course from the table view
-            Course selectedCourse = courseTable.getSelectionModel().getSelectedItem();
-            // Set the selected course in the model
+        Course selectedCourse = Vue.getCourseTable().getSelectionModel().getSelectedItem();
+
+        // Add event handler to courseTable
+        Vue.getCourseTable().setOnMouseClicked(event -> {
+            // Set the selected course in the Modele
             modele.setSelectedCourse(selectedCourse);
         });
 
-
-        // Display a success message (needs reworking)
-        RegistrationForm rf = (RegistrationForm) objectInputStream.readObject();
-        Course course = rf.getCourse();
-
-        String courseCode = course.getCode();
-        String firstName = rf.getPrenom();
-        String lastName = rf.getNom();
-
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Message");
-        alert.setHeaderText("Message");
-        alert.setContentText("Félicitations! " + firstName + " " + lastName + "est inscrit(e) avec succès pour le cours "
-                + courseCode + "!");
-
-        alert.showAndWait();
+        // If registration form is valid and a course is selected from table -> call Modele.registerStudent
+        if (Modele.validateForm() && selectedCourse != null) {
+            try {
+                modele.registerStudent(selectedSemester);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // Display success message
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Message");
+            alert.setHeaderText("Message");
+            alert.setContentText("Félicitations! " + firstName + " " + lastName +
+                    " est inscrit(e) avec succès pour le cours " + courseCode + "!");
+            alert.showAndWait();
+        } else {
+            // Display error message
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error");
+            alert.setContentText("Le formulaire est invalide");
+            alert.showAndWait();
+        }
     }
 }
